6350f540e6172ccdfee78172669f73dfb7c8ca96
Michelle Stermitz <stermitzm@gmail.com>
2020-08-18 14:15:26 -0700

    changed seed data, tables and load seed data

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'clean fridge',
+    completed: false,
+  },
+  {
+    todo: 'sweep',
+    completed: false,
+  },
+  {
+    todo: 'exercise',
+    completed: false,
+  }
+];
